2acc842a8990b1fb3fef727239e042ad251fb29c
tajrussell <tajrussell@cmu.edu>
2023-08-16 17:03:52 +0200

    adding zz_zjj_123j cross section

--- zhbb/get_cross_section.h
+++ zhbb/get_cross_section.h
@@ -6,7 +6,7 @@ double get_cross_section(const char *process_name) {
   else if (process_name == "wwjj_j") return 1.254;
   else if (process_name == "wzjj_j") return 0.2672;
   else if (process_name == "wz_wjj_123j") return 1.615;
-  else if (process_name == "zzjj_j") return 1;
-  else if (process_name == "zz_zjj_123j") return 1;
+  else if (process_name == "zzjj_j") return 0.0124;
+  else if (process_name == "zz_zjj_123j") return 0.4964;
   else return 1;
 }
