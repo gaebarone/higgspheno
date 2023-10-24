6115f3cb2a18bf7f6a592e9d28a6a0a4f76250c0
Gaetano Barone <gaetano.barone@cern.ch>
2023-10-24 10:58:28 -0400

    install script

--- install_mg.sh
+++ install_mg.sh
@@ -3,5 +3,18 @@ source /cvmfs/sft.cern.ch/lcg/releases/ROOT/6.14.04-0d8dc/x86_64-slc6-gcc7-opt/b
 #delphes setyp 
 here=${PWD}
 #change with your delphes dir
-#cd 
+cd ../
+mkdir -p mg 
+cd mg
+wget http://launchpad.net/madgraph5/3.0/3.5.x/+download/MG5_aMC_v3.5.1.tar.gz .
+tar -xvf MG5_aMC_v3.5.1.tar.gz
+cd MG5_aMC_v3.5.1 
+lhapdfDIR=`lhapdf-config --prefix`
+echo "set lhapdf $lhapdfDIR/bin/lhapdf-config" > startup.script
+echo "install Delphes" >> startup.scrpit 
+echo "install pythia8" >> startup.script 
+./bin/mg5_aMC startup.script 
+cd $here
+
+
 
--- setup_brux.sh
+++ setup_brux.sh
@@ -6,7 +6,8 @@ source /cvmfs/sft.cern.ch/lcg/views/LCG_101/x86_64-centos7-gcc11-opt/setup.sh
 #delphes setyp 
 here=${PWD}
 #change with your delphes dir
-cd /isilon/data/users/gbarone1/higgsandmore/code/mg/MG5_aMC_v3_5_1/Delphes
-source /isilon/data/users/gbarone1/higgsandmore/code/mg/MG5_aMC_v3_5_1/Delphes/DelphesEnv.sh
+#cd /isilon/data/users/gbarone1/higgsandmore/code/mg/MG5_aMC_v3_5_1/Delphes
+cd ../mg/MG5_aMC_v3_5_1/Delphes
+source ../mg/MG5_aMC_v3_5_1/Delphes/DelphesEnv.sh
 cd $here
 
