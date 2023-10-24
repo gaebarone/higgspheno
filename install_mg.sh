4a9a8e956280b82ab2e546051317e7ada0a84298
Gaetano Barone <gaetano.barone@cern.ch>
2023-10-24 11:13:52 -0400

    fix in the install script

--- install_mg.sh
+++ install_mg.sh
@@ -1,5 +1,4 @@
-source /cvmfs/sft.cern.ch/lcg/views/LCG_94/x86_64-slc6-gcc7-opt/setup.sh
-source /cvmfs/sft.cern.ch/lcg/releases/ROOT/6.14.04-0d8dc/x86_64-slc6-gcc7-opt/bin/thisroot.sh 
+source /cvmfs/sft.cern.ch/lcg/views/LCG_101/x86_64-centos7-gcc11-opt/setup.sh
 #delphes setyp 
 here=${PWD}
 #change with your delphes dir
