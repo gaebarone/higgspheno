b4eb6cf8e8147a229d3500ee87bf703cdbd8f5d7
Gaetano <gaetano.barone@cern.ch>
2023-11-13 17:43:05 +0100

    fix in instaler

--- install_mg.sh
+++ install_mg.sh
@@ -9,7 +9,7 @@ wget http://launchpad.net/madgraph5/3.0/3.5.x/+download/MG5_aMC_v3.5.1.tar.gz .
 tar -xvf MG5_aMC_v3.5.1.tar.gz
 cd MG5_aMC_v3_5_1
 lhapdfDIR=`lhapdf-config --prefix`
-echo "set lhapdf $lhapdfDIR/bin/lhapdf-config" > startup.script
+echo "set lhapdf $lhapdfDIR" > startup.script
 echo "install Delphes" >> startup.scrpit 
 echo "install pythia8" >> startup.script 
 ./bin/mg5_aMC startup.script 
