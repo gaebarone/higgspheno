0b33ec9b1dd2995fc8576f2480e107ad0e422701
sellissp <spencer_ellis@brown.edu>
2024-01-31 10:00:46 -0500

    updates

--- delphesAna/vvhjj/do_hadding.sh
+++ delphesAna/vvhjj/do_hadding.sh
@@ -11,15 +11,17 @@ hadd /isilon/data/users/sellis9/higgsandmore/delphesAna/vvhjj/rootOutputs/wzjj_j
 hadd /isilon/data/users/sellis9/higgsandmore/delphesAna/vvhjj/rootOutputs/wz_wjj_123j_${selection}.root wz_wjj_123j/${selection}_del*.root
 hadd /isilon/data/users/sellis9/higgsandmore/delphesAna/vvhjj/rootOutputs/zzjj_j_${selection}.root zzjj_j/${selection}_del*.root
 hadd /isilon/data/users/sellis9/higgsandmore/delphesAna/vvhjj/rootOutputs/zz_zjj_123j_${selection}.root zz_zjj_123j/${selection}_del*.root
-hadd /isilon/data/users/sellis9/higgsandmore/delphesAna/vvhjj/rootOutputs/diboson_${selection}.root /isilon/data/users/sellis9/higgsandmore/delphesAna/vvhjj/rootOutputs/${selection}_wwjj_j.root /isilon/data/users/sellis9/higgsandmore/delphesAna/vvhjj/rootOutputs/${selection}_wzjj_j.root /isilon/data/users/sellis9/higgsandmore/delphesAna/vvhjj/rootOutputs/${selection}_wz_wjj_123j.root /isilon/data/users/sellis9/higgsandmore/delphesAna/vvhjj/rootOutputs/${selection}_zzjj_j.root /isilon/data/users/sellis9/higgsandmore/delphesAna/vvhjj/rootOutputs/${selection}_zz_zjj_123j.root
 hadd /isilon/data/users/sellis9/higgsandmore/delphesAna/vvhjj/rootOutputs/drellyan_${selection}.root zll_123j/${selection}_del*.root
-hadd all_bkg_${selection}.root ttbar_${selection}.root ttHbb_${selection}.root diboson_${selection}.root drellyan_${selection}.root
+
+hadd /isilon/data/users/sellis9/higgsandmore/delphesAna/vvhjj/rootOutputs/diboson_${selection}.root /isilon/data/users/sellis9/higgsandmore/delphesAna/vvhjj/rootOutputs/wwjj_j_${selection}.root /isilon/data/users/sellis9/higgsandmore/delphesAna/vvhjj/rootOutputs/wzjj_j_${selection}.root /isilon/data/users/sellis9/higgsandmore/delphesAna/vvhjj/rootOutputs/wz_wjj_123j_${selection}.root /isilon/data/users/sellis9/higgsandmore/delphesAna/vvhjj/rootOutputs/zzjj_j_${selection}.root /isilon/data/users/sellis9/higgsandmore/delphesAna/vvhjj/rootOutputs/zz_zjj_123j_${selection}.root
+
+hadd /isilon/data/users/sellis9/higgsandmore/delphesAna/vvhjj/rootOutputs/all_bkg_${selection}.root /isilon/data/users/sellis9/higgsandmore/delphesAna/vvhjj/rootOutputs/ttbar_${selection}.root /isilon/data/users/sellis9/higgsandmore/delphesAna/vvhjj/rootOutputs/ttHbb_${selection}.root /isilon/data/users/sellis9/higgsandmore/delphesAna/vvhjj/rootOutputs/diboson_${selection}.root /isilon/data/users/sellis9/higgsandmore/delphesAna/vvhjj/rootOutputs/drellyan_${selection}.root
+
 
 hadd /isilon/data/users/sellis9/higgsandmore/delphesAna/vvhjj/rootOutputs/zzhqq_${selection}.root zzhqq/${selection}_del*.root
 hadd /isilon/data/users/sellis9/higgsandmore/delphesAna/vvhjj/rootOutputs/z0z0hqq_${selection}.root z0z0hqq/${selection}_del*.root
 hadd /isilon/data/users/sellis9/higgsandmore/delphesAna/vvhjj/rootOutputs/z0zThqq_${selection}.root z0zThqq/${selection}_del*.root
 hadd /isilon/data/users/sellis9/higgsandmore/delphesAna/vvhjj/rootOutputs/zTzThqq_${selection}.root zTzThqq/${selection}_del*.root
-
 
 hadd /isilon/data/users/sellis9/higgsandmore/delphesAna/vvhjj/rootOutputs/zzhqq_C3_1_${selection}.root zzhqq_C3_1/${selection}_del*.root
 hadd /isilon/data/users/sellis9/higgsandmore/delphesAna/vvhjj/rootOutputs/z0z0hqq_C3_1_${selection}.root z0z0hqq_C3_1/${selection}_del*.root
